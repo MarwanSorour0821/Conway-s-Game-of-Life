1f873f6af17cdaa041100257749c94b5f5436669
MarwanSorour0821 <117731750+MarwanSorour0821@users.noreply.github.com>
2024-02-21 11:37:52 +0000

    Add files via upload

---
+++ Parallel GoL/gol-skeleton-master/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"runtime"
+	"time"
+
+	"uk.ac.bris.cs/gameoflife/gol"
+	"uk.ac.bris.cs/gameoflife/sdl"
+)
+
+// main is the function called when starting Game of Life with 'go run .'
+func main() {
+
+	startTime := time.Now()
+
+	runtime.LockOSThread()
+	var params gol.Params
+
+	flag.IntVar(
+		&params.Threads,
+		"t",
+		8,
+		"Specify the number of worker threads to use. Defaults to 8.")
+
+	flag.IntVar(
+		&params.ImageWidth,
+		"w",
+		512,
+		"Specify the width of the image. Defaults to 512.")
+
+	flag.IntVar(
+		&params.ImageHeight,
+		"h",
+		512,
+		"Specify the height of the image. Defaults to 512.")
+
+	flag.IntVar(
+		&params.Turns,
+		"turns",
+		10000000000,
+		"Specify the number of turns to process. Defaults to 10000000000.")
+
+	noVis := flag.Bool(
+		"noVis",
+		false,
+		"Disables the SDL window, so there is no visualisation during the tests.")
+
+	flag.Parse()
+
+	fmt.Println("Threads:", params.Threads)
+	fmt.Println("Width:", params.ImageWidth)
+	fmt.Println("Height:", params.ImageHeight)
+
+	keyPresses := make(chan rune, 10)
+	events := make(chan gol.Event, 1000)
+
+	go gol.Run(params, events, keyPresses)
+	if !(*noVis) {
+		sdl.Run(params, events, keyPresses)
+	} else {
+		complete := false
+		for !complete {
+			event := <-events
+			switch event.(type) {
+			case gol.FinalTurnComplete:
+				complete = true
+			}
+		}
+	}
+
+	executionTime := time.Since(startTime)
+
+	fmt.Printf("Turns %d\n", params.Turns)
+	fmt.Printf("Workers%d\n", params.Threads)
+	fmt.Printf("Executiontime %s\n", executionTime)
+
+}
